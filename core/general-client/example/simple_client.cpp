86de897844df8f9268500b3f531b42405d2011a2
ShiningZhang <zhang_liang1991@126.com>
2021-08-25 09:54:41 +0000

    add simple_client for profile test

--- core/general-client/example/simple_client.cpp
+++ core/general-client/example/simple_client.cpp
@@ -14,8 +14,11 @@
 
 #include <fstream>
 #include <vector>
+#include <thread>
+#include <mutex>
 
 #include "core/general-client/include/brpc_client.h"
+#include "core/util/include/timer.h"
 
 using namespace std;  // NOLINT
 
@@ -23,22 +26,29 @@ using baidu::paddle_serving::client::ServingClient;
 using baidu::paddle_serving::client::ServingBrpcClient;
 using baidu::paddle_serving::client::PredictorInputs;
 using baidu::paddle_serving::client::PredictorOutputs;
+using baidu::paddle_serving::Timer;
 
 DEFINE_string(server_port, "127.0.0.1:9292", "");
 DEFINE_string(client_conf, "serving_client_conf.prototxt", "");
 DEFINE_string(test_type, "brpc", "");
 DEFINE_string(sample_type, "fit_a_line", "");
+DEFINE_int32(test_thread_num, 1, "");
+DEFINE_int32(test_count, 1, "");
 
 namespace {
-int prepare_fit_a_line(PredictorInputs& input, std::vector<std::string>& fetch_name) {
+int prepare_fit_a_line(PredictorInputs& input, std::vector<std::string>& fetch_name, int batch_size = 1) {
   std::vector<float> float_feed = {0.0137f, -0.1136f, 0.2553f, -0.0692f,
             0.0582f, -0.0727f, -0.1583f, -0.0584f,
             0.6283f, 0.4919f, 0.1856f, 0.0795f, -0.0332f};
-  std::vector<int> float_shape = {1, 13};
+  std::vector<float> float_feed_batch;
+  for (int i = 0; i < batch_size; ++i) {
+    float_feed_batch.insert(float_feed_batch.end(), float_feed.begin(), float_feed.end());
+  }
+  std::vector<int> float_shape = {batch_size, 13};
   std::string feed_name = "x";
   fetch_name = {"price"};
   std::vector<int> lod;
-  input.add_float_data(float_feed, feed_name, float_shape, lod);
+  input.add_float_data(float_feed_batch, feed_name, float_shape, lod);
   return 0;
 }
 
@@ -112,6 +122,28 @@ int prepare_bert(PredictorInputs& input, std::vector<std::string>& fetch_name) {
   fetch_name = {"pooled_output"};
   return 0;
 }
+
+double total_thread_cost = 0;
+int total_thread_count = 0;
+int batch_size = 1;
+std::mutex g_mutex;
+
+void thread_func(ServingClient* client, PredictorInputs input, std::vector<std::string> fetch_name, uint64_t log_id) {
+  Timer timeline;
+  timeline.Start();
+  for (int i = 0; i < FLAGS_test_count; ++i) {
+    PredictorOutputs output;
+    client->predict(input, output, fetch_name, log_id);
+    // LOG(INFO) << output.print();
+  }
+  timeline.Pause();
+  double cost = timeline.ElapsedMS();
+  LOG(INFO) << "thread[" << log_id << "]:"
+            << "total_cost=" << cost << "ms";
+  std::lock_guard<std::mutex> lck(g_mutex);
+  total_thread_cost += cost;
+  total_thread_count++;
+}
 } // namespace
 
 int main(int argc, char* argv[]) {
@@ -145,6 +177,10 @@ int main(int argc, char* argv[]) {
   if (sample_type == "fit_a_line") {
     prepare_fit_a_line(input, fetch_name);
   }
+  else if (sample_type == "fit_a_line_batch") {
+    prepare_fit_a_line(input, fetch_name, 1000);
+    batch_size = 1000;
+  }
   else if (sample_type == "bert") {
     prepare_bert(input, fetch_name);
   }
@@ -152,12 +188,46 @@ int main(int argc, char* argv[]) {
     prepare_fit_a_line(input, fetch_name);
   }
 
-  if (client->predict(input, output, fetch_name, 0) != 0) {
-    LOG(ERROR) << "Failed to predict!";
+  // if (client->predict(input, output, fetch_name, 0) != 0) {
+  //   LOG(ERROR) << "Failed to predict!";
+  // }
+  // else {
+  //   LOG(INFO) << output.print();
+  // }
+  std::vector<ServingClient*> vec_clients;
+  for (int i = 0; i < FLAGS_test_thread_num; ++i) {
+    ServingClient* client = new ServingBrpcClient();
+      if (client->init(confs, url) != 0) {
+      LOG(ERROR) << "Failed to init client!";
+      return 0;
+    }
+    vec_clients.push_back(client);
   }
-  else {
-    LOG(INFO) << output.print();
+
+  Timer total_timeline;
+  total_timeline.Start();
+  
+  std::vector<std::thread> vec_threads;
+  for (int i = 0; i < FLAGS_test_thread_num; ++i) {
+    vec_threads.push_back(std::thread(thread_func, vec_clients[i], input, fetch_name, i));
   }
+
+  for (std::thread& th : vec_threads) {
+    if (th.joinable()) {
+      th.join();
+    }
+  }
+
+  total_timeline.Pause();
+  int total_count = FLAGS_test_count;
+  double total_cost = total_timeline.ElapsedMS();
+  double each_thread_cost = total_thread_cost / total_thread_count;
+  double qps = (double)total_count * batch_size / each_thread_cost;
+
+  LOG(INFO) << "total cost: " << total_cost << "ms\n"
+            << "each thread cost: " << each_thread_cost << "ms\n"
+            << "qps: " << qps << "samples/s\n"
+            << "total count: " <<  total_count;
   
   return 0;
 }
